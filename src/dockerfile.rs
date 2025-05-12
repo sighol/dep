7d0316fb2a810ee61a6e9fd6a78dd3f248281629
Sigurd Holsen <sighol@gmail.com>
2025-05-13 00:46:40 +0200

    Report if the restart property is not set

--- src/dockerfile.rs
+++ src/dockerfile.rs
@@ -10,6 +10,7 @@ pub struct DockerFile {
 #[derive(Deserialize, Debug)]
 pub struct DockerService {
     pub build: Option<DockerBuild>,
+    pub restart: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -38,6 +39,16 @@ impl DockerContainer {
     pub fn from_docker_file(file: DockerFile) -> Vec<DockerContainer> {
         let mut output = vec![];
         for (service_name, service) in file.services.into_iter() {
+            match service.restart {
+                None => eprintln!(
+                    "\x1b[41;1mError\x1b[0m Service {service_name} does not have a `restart` property set"
+                ),
+                Some(x) => {
+                    if x != "unless-stopped" && x != "always" {
+                        eprintln!("\x1b[41;1mError\x1b[0m Service {service_name} has `restart` set to `{x}`")
+                    }
+                }
+            };
             if let Some(build_dir) = service.build {
                 if let DockerBuild::Str(s) = build_dir {
                     output.push(DockerContainer {
